daa2a0147270aa047b866a18641df618b37d5b03
Dev Soni <88739819+DevS0ni@users.noreply.github.com>
2024-01-17 23:17:44 -0500

    Add files via upload

---
+++ OOP244 - Final Milestone Project/Valet Parking Application/ms2/Utils.h
@@ -0,0 +1,31 @@
+/*
+-----------------------------------------------------------------------------
+ Author      : Dev Soni - dasoni4@myseneca.ca
+ Student #   : 130759210
+ Section     : NAA
+ MileStone2
+-----------------------------------------------------------------------------
+ */
+
+#ifndef SDDS_UTILS_H_
+#define SDDS_UTILS_H_
+
+#include <iostream>
+
+namespace sdds {
+    class Utils {
+    public:
+        int strlen(const char* str);
+        void strcpy(char* des, const char* src, int len = -1);
+        int strcmp(const char* s1, const char* s2);
+        // my functions - see Utils.cpp for more details
+        int getInt(int low, int high);
+        bool getYN();
+    };
+    extern Utils ut; // able to use utility members without 
+    // withoout instantiating - Only has to
+    // be done once at the beginning of 
+    // Utils.cpp
+}
+
+#endif // !SDDS_UTILS_H_
